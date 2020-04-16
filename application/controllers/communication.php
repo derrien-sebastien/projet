435e8453f14839cdf85c37d53e8d39c91890e461
kerenan <kerenan@hotmail.fr>
2020-04-16 09:52:18 +0200

    communication

--- application/controllers/communication.php
+++ application/controllers/communication.php
@@ -6,3 +6,6 @@ le model administrateur est inutile vu qu'on va viré la table
 le modele identifiant site est a vidé on fera les fonction dessus sous peu cela conserne uniquement le payement 
 les model pers parent et pers autre sont a supprimer pour ma par a chaque fois que je requette sur 
 les parent j'utilise au moins l'info noPersonne donc je fait tout dans le model personne 
+probleme je n'ai pas la vueCatalogue donc aucune possibilité de se connecter en visiteur
+la gestion de compte ne fonctionne plus en administrateur
+ 
